703e58a6416e033dee529048c481731a30673177
Oluwajoba Adegboye <oluwajobaadegboye@hotmail.com>
2018-10-22 12:00:00 -0500

    student and solution backend

--- routes/examTokens.js
+++ routes/examTokens.js
@@ -2,12 +2,11 @@ var express = require('express');
 var router = express.Router();  
 var uuid = require('uuid');
 var random = require('mongoose-random');
-
-
-
+ 
 /* GET examTokens listing. */
 router.get('/',function(req, res) { 
     req.mongoose.model('ExamToken').find({},function(err,examTokens){ 
+      if(err) return res.status(500).send({"responseMessage":"Something went wrong"})
       res.send(examTokens); 
   });
 });
@@ -15,6 +14,7 @@ router.get('/',function(req, res) {
 /* GET inactive examTokens. */
 router.get('/inactive', function(req, res) { 
   req.mongoose.model('ExamToken').find({"status":"inactive"},function(err,examTokens){
+    if(err) return res.status(500).send({"responseMessage":"Something went wrong"})
     res.send(examTokens);
   })
 });
@@ -29,19 +29,26 @@ router.get('/active', function(req, res) {
 /* GET examTokens with specified Id. */
 router.get('/:id', function(req, res) { 
   req.mongoose.model('ExamToken').findOne({_id:req.params.id},function(err,examToken){
+    if(err) return res.status(500).send({"responseMessage":"Something went wrong"})
     res.send(examToken);
   })
 });
 
 /* GET exam questions */
 router.get('/questions/:token/:studentId', function(req, res) { 
-  
+    var filter = { status: "active" };
+    var fields = { question: 1,status:1 };
+    var options = { limit: 3};
+    req.mongoose.model('Question').findRandom(filter, fields, options, function(err, questions) {
+      if(err) return res.status(500).send({"responseMessage":"Something went wrong"})
+      res.status(200).send(questions)
+    });
 });
 
 /* GET examTokens status by Id. */
 router.get('/verify/status/:token', function(req, res) { 
   req.mongoose.model('ExamToken').findOne({token:req.params.token},function(err,examToken){
-    if(err) return res.send(err)
+    if(err) return res.status(500).send({"responseMessage":"Something went wrong"})
     res.send({"isActive":examToken.status=="active","firstName":examToken.student.firstName,"studentId":examToken.student._id});
   })
 });
@@ -58,7 +65,7 @@ router.post('/', function(req, res) {
 
     req.mongoose.model('Student').findOne({_id:req.body.studentId},function(err,student){
         if(err || student==null){
-
+          if(err) return res.status(500).send({"responseMessage":"Something went wrong"})
         }
         examTokenObj.student = student
         examTokenObj.save((err)=>{ 
@@ -66,7 +73,7 @@ router.post('/', function(req, res) {
             const response =err.code==11000? {"responseCode":"10","responseMessage":"Duplicate username and/or email"}:err;
             return  res.status(403).send(response);
           } 
-            return res.status(200).send(examTokenObj);
+          return res.status(200).send(examTokenObj);
         });
     })
     
@@ -77,9 +84,8 @@ router.post('/', function(req, res) {
 /* Toggle examTokens status */
 router.patch('/status/:id/:status', function(req, res) {
   req.mongoose.model('ExamToken').findByIdAndUpdate({"_id":req.params.id},{$set:{"status":req.params.status}},
-  (err, examTokens) => { 
-        if (err) return res.status(500).send(err); 
-         
+  (err, examTokens) => {  
+        if(err) return res.status(500).send({"responseMessage":"Something went wrong"})
         return res.send({"responseCode":"00","responseMessage":"Update successful"})
     }
   );
